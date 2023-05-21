e76e2c1047f83142b307cc8a5dae7ceeb9a91b52
Patrick Mc Cluskey <patrick.mc.com@gmail.com>
2023-05-21 12:00:21 -0300

    Cambio del nombre del archivo de paso 1 a encripta.dor

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function btnEncriptar(){
 }
 
 function encriptar(stringEncriptada){
-    let matrizCodigo = [["a","xdhn"],["e","otgk"],["i","htnb"],["o","vews"],["u","glfd"]];
+    let matrizCodigo = [["a",".1001."],["e",".0101."],["i",".1010."],["o",".0110."],["u",".0011."]];
     stringEncriptada = stringEncriptada.toLowerCase()
 
     for(let i = 0; i < matrizCodigo.length; i++){
@@ -20,3 +20,33 @@ function encriptar(stringEncriptada){
     return stringEncriptada;
 
 }
+
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value)
+    resultado.value = textoEncriptado  
+    textArea.value = ""; 
+}
+
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["a",".1001."],["e",".0101."],["i",".1010."],["o",".0110."],["u",".0011."]];
+    stringDesencriptada = stringDesencriptada.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+
+    }
+    return stringDesencriptada;
+
+}
+
+function copiarAlPortapapeles() {
+    var texto = document.querySelector(".Resultado").value;
+    var elementoTemporal = document.createElement("textarea");
+    elementoTemporal.value = texto;
+    document.body.appendChild(elementoTemporal);
+    elementoTemporal.select();
+    document.execCommand("copy");
+    document.body.removeChild(elementoTemporal);
+  }
